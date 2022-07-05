2147984e9e2b4ab6ccaecb33fd432024630ba8c4
Tux7P <55027789+tux7P@users.noreply.github.com>
2022-07-05 13:03:53 +0300

    🚧 tests: add ui and api spec

---
+++ cypress/tests/api/customer_api.cy.ts
@@ -0,0 +1,18 @@
+describe('Customer - API Tests', () => {
+    
+    it('Create a customer', () => {
+   
+    })
+    
+    it('List all customers', () => {
+    
+    })
+    
+    it('Update a customer', () => {
+       
+    })
+    
+    it('Delete a customer', () => {
+
+    })
+})
---
+++ cypress/tests/ui/customer.cy.ts
@@ -0,0 +1,14 @@
+describe('Customer - Functional Tests', () => {
+    
+  it('Add customer data', () => {
+    cy.visit('/')
+  })
+  
+  it('Update customer data', () => {
+  
+  })
+  
+  it('Delete customer data', () => {
+     
+  })
+})
