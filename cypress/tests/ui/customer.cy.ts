71cf7c54d3865528164ec2dca5c5d6b19de55e39
Tux7P <55027789+tux7P@users.noreply.github.com>
2022-07-08 12:56:15 +0300

    ⚡️ test: add two other stubbing tests

--- cypress/tests/ui/customer.cy.ts
+++ cypress/tests/ui/customer.cy.ts
@@ -27,22 +27,55 @@ let billing: IBilling = {
 let id: number
 let CUSTOMER_INTERCEPT_URL: string = 'https://customers.katanamrp.com/api/customers'
 
+
 describe('Customer - Functional Tests', () => {
-    
-  it('Incercept and validate backend call from frontend', () => {
-    cy.intercept('POST', CUSTOMER_INTERCEPT_URL).as("InterceptCustomerData")   
-    cy.login()     
-    customerPage.goToAddNewCustomer()
-    customerPage.fillCustomerData(customer.firstname, customer.lastname, customer.company, customer.email, customer.phone, customer.comment)
-    customerPage.fillBillingDetails(billing.billingfirstname, billing.billinglastname, billing.billingcompany, billing.billingphone, billing.billingapartment, billing.billingcity, billing.billingregion, billing.billingzip, billing.billingcountry)
-    cy.wait('@InterceptCustomerData')   
-        .then(({response}) => {
-            expect(response.statusCode).to.eq(200);
-            cy.log("New Customer ID: "+ response.body.id)
-            id = response.body.id
-            expect(response.body.firstName).to.eq(customer.firstname)
-            expect(response.body.name).to.eq(customer.firstname)
-            expect(response.body.currency).to.eq('USD')
-        })
+  beforeEach( () => {
+   cy.login()
   })
+
+   it('Incercept and validate backend call from frontend', () => {
+       cy.intercept('POST', CUSTOMER_INTERCEPT_URL).as("InterceptCustomerData")        
+       customerPage.goToAddNewCustomer()
+       customerPage.fillCustomerData(customer.firstname, customer.lastname, customer.company, customer.email, customer.phone, customer.comment)
+       customerPage.fillBillingDetails(billing.billingfirstname, billing.billinglastname, billing.billingcompany, billing.billingphone, billing.billingapartment, billing.billingcity, billing.billingregion, billing.billingzip, billing.billingcountry)
+       cy.wait('@InterceptCustomerData')   
+           .then(({response}) => {
+               expect(response.statusCode).to.eq(200);
+               cy.log("New Customer ID: "+ response.body.id)
+               id = response.body.id
+               expect(response.body.firstName).to.eq(customer.firstname)
+               expect(response.body.name).to.eq(customer.firstname)
+               expect(response.body.currency).to.eq('USD')
+           })
+   })
+
+   it('Stubbing a response body and validate the response', () => {
+       cy.intercept('POST', CUSTOMER_INTERCEPT_URL, {body: []}).as("StubbedEmptyBody")
+       customerPage.goToAddNewCustomer()
+       customerPage.fillCustomerData(customer.firstname, customer.lastname, customer.company, customer.email, customer.phone, customer.comment)
+       customerPage.fillBillingDetails(billing.billingfirstname, billing.billinglastname, billing.billingcompany, billing.billingphone, billing.billingapartment, billing.billingcity, billing.billingregion, billing.billingzip, billing.billingcountry)
+       cy.wait('@StubbedEmptyBody')   
+         .then(({response}) => {
+               expect(response.statusCode).to.eq(200)
+         })
+   })
+
+   it('Stubbing a response status without touching server and validate the response', () => {
+       cy.intercept('POST', CUSTOMER_INTERCEPT_URL,{forcedNetworkError: true}).as("StubbedNetworkError")
+       customerPage.goToAddNewCustomer()
+       customerPage.fillCustomerData(customer.firstname, customer.lastname, customer.company, customer.email, customer.phone, customer.comment)
+       customerPage.fillBillingDetails(billing.billingfirstname, billing.billinglastname, billing.billingcompany, billing.billingphone, billing.billingapartment, billing.billingcity, billing.billingregion, billing.billingzip, billing.billingcountry)
+       cy.wait('@StubbedNetworkError')   
+           .then(({response}) => {
+               expect(response.statusCode).to.eq(200);
+               expect(response.body.status).to.eq(undefined);
+               cy.log(response.body)
+               cy.log(response.body.status)
+           })
+   })
+
+  afterEach(() => {
+      cy.logout() 
+   })
 })
+
