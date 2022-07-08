ee671b25630ef69273d3b593c4e453385a94c38b
Tux7P <55027789+tux7P@users.noreply.github.com>
2022-07-08 10:01:12 +0300

    🎨 test: add more tests for invalid login

--- cypress/tests/ui/login.cy.ts
+++ cypress/tests/ui/login.cy.ts
@@ -1,6 +1,17 @@
+import {loginPage} from '../../support/PageObjects/LoginPage';
+
 describe('Login Tests', () => {
   it('Successfull login & logout', () => {
     cy.login(Cypress.env("correct_email"), Cypress.env("correct_password"));
     cy.logout()
+  })
+  it('Unsuccessfull login with incorrect email', () => {
+    cy.login(Cypress.env('incorrect_email'), Cypress.env('correct_password'))
+    loginPage.verifyErrorMessage()
+  })
+  
+  it('Unsuccessfull login with incorrect password', () => {
+    cy.login(Cypress.env('correct_email'), Cypress.env('incorrect_password'))
+    loginPage.verifyErrorMessage()
   })
 })
