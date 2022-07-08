d9d3b4702fb2b76a4ada7d7ac02a1eded49b6723
Tux7P <55027789+tux7P@users.noreply.github.com>
2022-07-08 09:53:12 +0300

    ⚡️ feat: add interfaces for customer and billing

---
+++ cypress/support/Interfaces/IBilling.ts
@@ -0,0 +1,11 @@
+export interface IBilling {
+    billingfirstname: string
+    billinglastname: string
+    billingcompany: string
+    billingphone: string
+    billingapartment: string
+    billingcity: string
+    billingregion: string
+    billingzip: string
+    billingcountry: string
+}
